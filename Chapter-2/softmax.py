9c16cff27ea254fa0ee23bb737f4eeb98f3e9f3e
hiro <69237236+hir777@users.noreply.github.com>
2021-08-03 22:22:21 +0900

    Update softmax.py

--- Chapter-2/softmax.py
+++ Chapter-2/softmax.py
@@ -1,8 +1,14 @@
 import numpy as np
 
 def softmax(a):
-    exp_a = np.exp(a)
+    c = np.max(a)
+    exp_a = np.exp(a - c)       # subtract c from a to prevent an overflow
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
 
     return y
+
+a = np.array([0.3, 2.9, 4.0])
+y = softmax(a)
+print(y)            # [0.01821127 0.24519181 0.73659691]
+print( np.sum(y) )  # 1.0
